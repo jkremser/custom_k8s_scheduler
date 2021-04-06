9d5afd05e77c8cffa4b363252bd44418b8a6f5bb
Jirka Kremser <jiri.kremser@oracle.com>
2021-04-06 16:47:21 +0200

    example: list pods

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #![allow(unused_imports, unused_variables)]
-pub use kube_runtime::controller::*;
+use k8s_openapi::api::core::v1::Pod;
 use prometheus::{Encoder, TextEncoder};
 use std::cell::Cell;
 use tracing::{debug, error, info, trace, warn};
+
+use kube::{
+    api::{Api, DeleteParams, ListParams, Patch, PatchParams, PostParams, Resource, WatchEvent},
+    Client,
+};
 
 struct SchedulerData {
     requests: Cell<usize>,
@@ -28,19 +33,28 @@ async fn health(_: HttpRequest) -> impl Responder {
 #[get("/")]
 async fn index(_req: HttpRequest) -> impl Responder {
     // let state = c.state().await;
+    let client: Client = Client::try_default().await.expect("Unable to initialize the k8s client, is Kubernetes up and running?");
+    let pods: Api<Pod> = Api::namespaced(client, "kube-system");
+    let lp = ListParams::default();
+    for p in pods.list(&lp).await {
+        info!("Found Pod: {:?}", &p);
+        // info!("Found Pod: {}", Resource::name(&p));
+    }
     HttpResponse::Ok().json("ok")
 }
 
 #[actix_rt::main]
-async fn main() -> Result<(), ()> {
-// fn main() {
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=debug");
+
+
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .init();
-    let client = kube::Client::try_default().await.expect("create client");
+
     // let (manager, drainer) = Manager::new(client).await;
 
-    let server = HttpServer::new(move || {
+    let server = HttpServer::new(move || {  
         App::new()
             .data(SchedulerData{ requests: Cell::new(0) })
             .wrap(middleware::Logger::default().exclude("/health"))
@@ -51,6 +65,9 @@ async fn main() -> Result<(), ()> {
     .bind("0.0.0.0:8080")
     .expect("Can not bind to 0.0.0.0:8080")
     .shutdown_timeout(0);
+
+
+
 
     tokio::select! {
         // _ = drainer => warn!("controller drained"),
